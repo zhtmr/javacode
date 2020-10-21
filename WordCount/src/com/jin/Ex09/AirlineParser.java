2f34abd0ea23b85a3773830c96017feeb4ecdf9f
zhtmr <zhtmr123@gmail.com>
2020-10-21 16:45:02 +0900

    1021f

---
+++ WordCount/src/com/jin/Ex09/AirlineParser.java
@@ -0,0 +1,67 @@
+package com.jin.Ex09;
+
+import org.apache.hadoop.io.Text;
+
+public class AirlineParser {
+	private int year;
+	private int month;
+	private int dayOfweek;
+	private int depTime;
+	private int dayOfmonth;
+	private int cancelled;
+	private String uniqueCarrier;
+	private int carrierDelay;
+	final static int SUSPENSIONOFAIRLINE = -1;
+	final static int NONCANCELLED = 0;
+	
+	private int getDigitFromStr(String data, int def) {
+		if("na".equalsIgnoreCase(data)) return def;
+		return Integer.parseInt(data);
+	}
+	
+	public AirlineParser(Text txt) {
+		String[] airdata = txt.toString().split(",");
+		year = Integer.parseInt(airdata[0]);
+		month = Integer.parseInt(airdata[1]);
+		dayOfmonth=Integer.parseInt(airdata[2]);
+		dayOfweek = Integer.parseInt(airdata[3]);
+		depTime = getDigitFromStr(airdata[4],SUSPENSIONOFAIRLINE);
+		cancelled = Integer.parseInt(airdata[21]);
+		uniqueCarrier=airdata[14];
+		carrierDelay=getDigitFromStr(airdata[24], SUSPENSIONOFAIRLINE);
+		
+	}
+
+	public int getDayOfmonth() {
+		return dayOfmonth;
+	}
+
+	public int getCancelled() {
+		return cancelled;
+	}
+	public int getYear() {
+		return year;
+	}
+	public int getCarrierDelay() {
+		return carrierDelay;
+	}
+	public String getUniqueCarrier() {
+		return uniqueCarrier;
+	}
+	public int getDayOfweek() {
+		return dayOfweek;
+	}
+	public int getDeptime() {
+		return depTime;
+	}
+
+
+
+	public int getMonth() {
+		return month;
+	}
+
+	
+	
+	
+}
---
+++ WordCount/src/com/jin/Ex09/Reduce.java
@@ -0,0 +1,40 @@
+package com.jin.Ex09;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter;
+
+public class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, Text>{
+	
+	
+	
+	private String getTotalCnt(Iterator<IntWritable> values) {
+		int cnt =0;
+		int sum=0;
+		while(values.hasNext()) {
+			sum += values.next().get();
+			cnt++;
+		}
+		return sum+","+cnt;
+	}
+	
+	@Override
+	public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, Text> output, Reporter reporter)
+			throws IOException {
+		
+		String[] split = getTotalCnt(values).split(",");
+		int sum = Integer.parseInt(split[0]);
+		int cnt = Integer.parseInt(split[1]);
+		float avg = (float)sum/(float)cnt;
+		output.collect(key, new Text(sum+","+avg));
+		
+
+	}
+
+}
