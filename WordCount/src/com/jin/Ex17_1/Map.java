daf064194fb3cd64dde5e6f3dde968bf0dd4171b
sungwon <48509269+zhtmr@users.noreply.github.com>
2020-10-25 19:33:34 +0900

    1025

--- WordCount/src/com/jin/Ex17_1/Map.java
+++ WordCount/src/com/jin/Ex17_1/Map.java
@@ -15,7 +15,9 @@ public class Map extends Mapper<LongWritable, Text, PartSort, IntWritable>{
 //		AirlineParser parser = new AirlineParser(value);
 		
 		
-		// 정렬을 PartSort에서 정의한 방식대로 하겠다
+		/* 정렬을 PartSort에서 정의한 방식대로 하겠다
+		 * 직접 데이터 타입을 정의하고 싶다면 WritableComparable 인터페이스를 구현한다.
+		 */
 		PartSort parser = new PartSort(value);
 		if(!parser.isCancelled()) {
 			context.write(parser, new IntWritable(1));
